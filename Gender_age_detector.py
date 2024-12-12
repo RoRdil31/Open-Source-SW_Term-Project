7a5db7e0486d660fa367fec97a5ae81e2756eb29
Minji Kim <rlaalswl1151@naver.com>
2024-12-12 14:52:42 +0900

    add MODEL_MEAN_VALUES

--- Gender_age_detector.py
+++ Gender_age_detector.py
@@ -3,7 +3,8 @@ import cv2
 video_capture = cv2.VideoCapture(0)
 faceCascade = cv2.CascadeClassifier('style.xml')  # 얼굴 인식에 사용할 Cascade 분류기 로드
 video_capture.set(3, 480)  # 프레임의 너비 설정
-video_capture.set(4, 640)  # 프레임의 높이 설정MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+video_capture.set(4, 640)  # 프레임의 높이 설정
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 age_list = ['(0, 2)', '(4, 6)', '(8, 12)', '(15, 20)', '(25, 32)', '(38, 43)', '(48, 53)', '(60, 100)']
 gender_list = ['Male', 'Female']
 
